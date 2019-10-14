0c95982d8c1b5c2b5f975488de6ca1c613021dae
Remmy75 <46570070+Remmy75@users.noreply.github.com>
2019-10-14 10:32:15 -0500

    Add files via upload

---
+++ Conference_Scheduler/model/database_error.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <!-- the head section -->
+    <head>
+        <title>Conference Scheduler</title>
+    </head>
+
+    <!-- the body section -->
+    <body>
+    <main>
+        <h1>Database Error</h1>
+        <p>There was an error connecting to the database.</p>
+        <p>The database must be installed as described in the appendix.</p>
+        <p>Error message: <?php echo $error_message; ?></p>
+    </main>
+    </body>
+</html>
---
+++ Conference_Scheduler/model/speakers_db.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of speakers_db
+ *
+ * @author Home
+ */
+class speakers_db {
+    //put your code here
+}
